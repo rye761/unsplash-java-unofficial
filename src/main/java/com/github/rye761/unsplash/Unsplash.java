dc82a13637a799041f9b78b91cbd6475057baf75
rye761 <ryebread761@gmail.com>
2016-06-25 14:36:53 -0600

    Allow for user login.

--- src/main/java/com/github/rye761/unsplash/Unsplash.java
+++ src/main/java/com/github/rye761/unsplash/Unsplash.java
@@ -3,17 +3,22 @@ package com.github.rye761.unsplash;
 import java.io.IOException;
 import java.util.Map;
 import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
+import com.github.scribejava.core.utils.OAuthEncoder;
 import com.google.gson.Gson;
 
 public final class Unsplash {
     private static Unsplash INSTANCE;
     public static final String BASE_URL = "https://api.unsplash.com/";
     public static final Gson GSON = new Gson();
-    private String clientId = null;
+    private String clientId;
+    private String apiSecret;
+    private String callback;
     private OAuth20Service service;
+    private OAuth2AccessToken token;
 
     private Unsplash() {
 
@@ -27,16 +32,55 @@ public final class Unsplash {
     }
 
     public void init(Map<String, String> config) {
-        final String apiKey = config.get("applicationId");
-        final String apiSecret = config.get("secret");
-        final String callback = config.get("callbackUrl");
-        clientId = apiKey;
+        clientId = config.get("applicationId");
+        apiSecret = config.get("secret");
+        callback = config.get("callbackUrl");
+        final String accessToken = config.get("accessToken");
+        if (accessToken != null) {
+            token = new OAuth2AccessToken(accessToken);
+        }
 
         service = new ServiceBuilder()
-                    .apiKey(apiKey)
+                    .apiKey(clientId)
                     .apiSecret(apiSecret)
                     .callback(callback)
                     .build(UnsplashApi.instance());
+    }
+    
+    public String getAuthorizationUrl(String[] scopes) {
+        final String scopeString = makeScopeString(scopes);
+        System.out.println("Scope string is: " + scopeString);
+        service = new ServiceBuilder()
+                .apiKey(clientId)
+                .apiSecret(apiSecret)
+                .callback(callback)
+                .scope(scopeString)
+                .build(UnsplashApi.instance());       
+//      I have to decode the URL, the unsplash API complains of an invalid 
+//      scope if the URL remains encoded. I'd like to find a better solution
+//      in the future but for now this works.
+        return OAuthEncoder.decode(service.getAuthorizationUrl());
+    }
+    
+    public String generateToken(String authCode) {
+        try {
+            token = service.getAccessToken(authCode);
+            return token.getAccessToken();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private String makeScopeString(String[] scopes) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < scopes.length; i++) {
+            builder.append(scopes[i]);
+            if (i != (scopes.length - 1)) {
+                builder.append("+");
+            }
+        }
+        return builder.toString();
     }
 
     private String makeUrl(String endpoint, Map<String, String> params) {
@@ -60,6 +104,10 @@ public final class Unsplash {
     public String request(Verb verb, String endpoint, Map<String, String> params) {
         final String url = makeUrl(endpoint, params);
         final OAuthRequest req = new OAuthRequest(verb, url, service);
+        
+        if (token != null) {
+            service.signRequest(token, req);
+        }
 
         try {
             return req.send().getBody();
