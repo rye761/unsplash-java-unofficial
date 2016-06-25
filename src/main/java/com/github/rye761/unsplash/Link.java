6a2a60f9fe734f4eca3844df6c6215a6f07c916e
rye761 <ryebread761@gmail.com>
2016-06-25 00:02:12 -0600

    Complete definition of Link

--- src/main/java/com/github/rye761/unsplash/Link.java
+++ src/main/java/com/github/rye761/unsplash/Link.java
@@ -4,12 +4,18 @@ public class Link {
     public final String self;
     public final String html;
     public final String download;
+    public final String photos;
+    public final String related;
 
     public Link(String self,
             String html,
-            String download) {
+            String download,
+            String photos,
+            String related) {
         this.self = self;
         this.html = html;
         this.download = download;
+        this.photos = photos;
+        this.related = related;
     }
 }
